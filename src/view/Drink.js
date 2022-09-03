e21b061cda52a32ceecfe088ab403e189b2f2e64
warat09 <y-title@hotmail.com>
2022-09-04 02:56:55 +0700

    Realtime

--- src/view/Drink.js
+++ src/view/Drink.js
@@ -1,12 +1,40 @@
 import * as React from "react";
 import emailjs from 'emailjs-com';
+import { render } from "@testing-library/react";
 
 
-
+// function getRealTime() {
+//     const currentTime = Date.now();
+//     var d = new Date(currentTime)
+//     var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+//     console.log(time)
+//     if(d.getMinutes() == "27" && d.getSeconds() == "0"){
+//         console.log("hello")
+//     }
+//     return (Math.floor(currentTime / 1000) + 1) * 1000 - currentTime;
+//   }
+  
+//   (async function () {
+//     let reduceTime = 0;
+//     while (true) {
+//       reduceTime = getRealTime();
+//       await sleep(reduceTime);
+//     }
+//   })()
+  
+//   function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
 async function sendEmail(e){
     e.preventDefault();
+    var templateParams = {
+        name: 'ytitile7797@gmail.com',
+        subject: 'Check this out!',
+        message:'hello sig'
 
-    emailjs.sendForm('service_9y5vii1', 'template_y5p8guz', e.target, '0WTwQ785q4wjqSYDp')
+    };
+
+    emailjs.send('service_9y5vii1', 'template_y5p8guz', templateParams, '0WTwQ785q4wjqSYDp')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,4 +1,52 @@
 import * as React from "react";
+import emailjs from 'emailjs-com';
+function getRealTime() {
+  const currentTime = Date.now();
+  var d = new Date(currentTime)
+  var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  console.log(time)
+  if(d.getMinutes() == "48" && d.getSeconds() == "0"){
+      console.log("hello")
+      var templateParams = {
+        name: 'ytitile7797@gmail.com',
+        subject: 'Check this out!',
+        message:'hello sig'
+
+    };
+
+    emailjs.send('service_9y5vii1', 'template_y5p8guz', templateParams, '0WTwQ785q4wjqSYDp')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+  }
+  return (Math.floor(currentTime / 1000) + 1) * 1000 - currentTime;
+}
+
+(async function () {
+  let reduceTime = 0;
+  while (true) {
+    reduceTime = getRealTime();
+    await sleep(reduceTime);
+  }
+})()
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function sendEmail(e){
+  e.preventDefault();
+  console.log(e.target)
+
+  // emailjs.sendForm('service_9y5vii1', 'template_y5p8guz', e.target, '0WTwQ785q4wjqSYDp')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  //     e.target.reset()
+}
 
 function Home() {
   return (
