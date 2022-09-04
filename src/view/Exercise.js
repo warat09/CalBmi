da5e3fa403e418080e4ddbd267b77d6852347bc3
warat09 <y-title@hotmail.com>
2022-09-05 03:19:58 +0700

    Timer

---
+++ src/view/Exercise.js
@@ -0,0 +1,92 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+function Exercise() {
+    
+    const Ref = useRef(null);
+  
+    const [timer, setTimer] = useState('00:01:00');
+    const [Second, setSecond] = useState(0);
+
+  
+  
+    const getTimeRemaining = (e) => {
+        const total = Date.parse(e) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+        return {
+            total, hours, minutes, seconds
+        };
+    }
+  
+  
+    const startTimer = (e) => {
+        let { total, hours, minutes, seconds } 
+                    = getTimeRemaining(e);
+                    setSecond(total/1000)
+        if (total >= 0) {
+  
+            setTimer(
+                (hours > 9  ? hours : '0' + hours) + ':' +
+                (minutes > 9 ? minutes : '0' + minutes) + ':'
+                + (seconds > 9 ? seconds : '0' + seconds)
+            )
+        }
+    }
+    
+    const clearTimer = (e) => { 
+        setTimer('00:01:00');
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e);
+        }, 1000)
+        Ref.current = id;
+    }
+  
+    const getDeadTime = () => {
+        let deadline = new Date();
+        deadline.setSeconds(deadline.getSeconds() + 60);
+        return deadline;
+    }
+    const getDeadTimeTo = () => {
+        let deadline = new Date();
+        deadline.setSeconds(deadline.getSeconds() + Second);
+        return deadline;
+    }
+    const startDeadTime = (e) => {
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e);
+        }, 1000)
+        Ref.current = id;
+    }
+    const onClickStart = () => {
+        clearTimer(getDeadTime());
+    }
+    const onClickStop = () => {
+        clearInterval(Ref.current);
+    }
+  
+    // Another way to call the clearTimer() to start
+    // the countdown is via action event from the
+    // button first we create function to be called
+    // by the button
+    const onClickReset = () => {
+        clearTimer(getDeadTime());
+    }
+    const onClickStarto = () => {
+        startDeadTime(getDeadTimeTo());
+    }
+  
+    return (
+        <div className="App">
+            <h2>{timer}</h2>
+            <button onClick={onClickStart}>Start</button>
+            <button onClick={onClickStarto}>Startต่อ</button>
+            <button onClick={onClickStop}>Stop</button>
+            <button onClick={onClickReset}>Reset</button>
+        </div>
+    )
+}
+
+export default Exercise;
