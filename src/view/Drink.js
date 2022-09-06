7de87b869e897b8b7cef42583c1004ef9d6bcce6
BuildTools <unconfigured@null.spigotmc.org>
2022-09-06 11:52:03 +0700

    add water bottle

--- src/view/Drink.js
+++ src/view/Drink.js
@@ -1,15 +1,41 @@
 import React, { useState,useEffect } from 'react'
 import emailjs from 'emailjs-com';
-
+import emptybottle from './../images/water.png'
+import water5 from './../images/water5.png'
+import water4 from './../images/water4.png'
+import water3 from './../images/water3.png'
+import water2 from './../images/water2.png'
+import water1 from './../images/water1.png'
+import Drinkcss from './../css/Drinkcss.css'
 function Drink() {
     var firsttime = new Date(Date.now());
     const [date, setDate] = useState(firsttime.getHours() + ":" +firsttime.getMinutes() + ":" +firsttime.getSeconds());
+    const [date2,setdate2] = useState(0)
+    const [sleep,setsleep] =useState(false)
     function getRealTime() {
         const currentTime = Date.now();
         var d = new Date(currentTime)
-        var time = d.getHours() + ":" +d.getMinutes() + ":" +d.getSeconds();
+        if(parseInt(d.getSeconds())<10){
+          if(parseInt(d.getMinutes())<10){
+            var time = d.getHours() + ":0" +d.getMinutes() + ":0" +d.getSeconds();
+            
+          }
+          else{
+            var time = d.getHours() + ":" +d.getMinutes() + ":0" +d.getSeconds();
+          }
+        }
+        else{
+          if(parseInt(d.getMinutes())<10){
+            var time = d.getHours() + ":0" +d.getMinutes() + ":" +d.getSeconds();
+            
+          }
+          else{
+            var time = d.getHours() + ":" +d.getMinutes() + ":" +d.getSeconds();
+          }
+        }
+        
         setDate(time)
-        if(d.getMinutes() == "58" && d.getSeconds() == "0"){
+        if(d.getMinutes() == "58" && d.getSeconds() == "00"){
             console.log("hello")
             var templateParams = {
                 name: 'ytitile7797@gmail.com',
@@ -33,11 +59,52 @@ function Drink() {
           clearInterval(timerID);
         };
       }, []);
+    useEffect(()=>{
+      setdate2(parseInt(date.split(":")[0].concat(date.split(":")[1].concat(date.split(":")[2]))))
+      if(date2==200001){
+        setsleep(true)
+      }
+      else if(date2==80000){
+        setsleep(false)
+      }
+    },[date])
   return (
 <div>
     <div className="container">
         <h1>Drink</h1>
-        <h1>{date}</h1>
+        
+     
+        {date2>=80000&&date2<110000&&
+          <div className='waterbottlediv'>
+            <img src={water4} className='waterbottle' alt="logo"/>
+          </div>
+        }
+        {date2>=110000&&date2<140000&&
+          <div className='waterbottlediv'>
+            <img src={water3} className='waterbottle' alt="logo"/>
+          </div>
+        }
+        {date2>0&&date2<80000&&sleep==true&&
+          <div className='waterbottlediv'>
+            <img src={water5} className='waterbottle' alt="logo"/>
+          </div>
+        }
+        {date2>=140000&&date2<170000&&
+          <div className='waterbottlediv'>
+            <img src={water2} className='waterbottle' alt="logo"/>
+          </div>
+        }
+        {date2>=170000&&date2<200000&&
+          <div className='waterbottlediv'>
+            <img src={water1} className='waterbottle' alt="logo"/>
+          </div>
+        }
+        {date2>=200000&&date2<240000&&sleep==false&&
+          <div className='waterbottlediv'>
+            <img src={emptybottle} className='waterbottle' alt="logo"/>
+          </div>
+        }
+           <h1>{date}</h1>
     </div>
 </div>
   );
